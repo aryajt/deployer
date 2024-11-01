558d8a9b404d763c4106d9807c8284b071ec385d
arya <javdani.arya@gmail.com>
2024-11-02 00:19:21 +0330

    .

---
+++ deployer.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const { exec } = require("child_process");
+
+const app = express();
+const PORT = 3001;
+
+
+app.get("/deploy/shop", (req, res) => {
+    const pm2Name = 'hello'
+    const branchName = 'test';
+    const projectDir = "";
+    exec(`cd ${projectDir} && git fetch`, (fetchError) => {
+        if (fetchError) {
+            return res.status(500).send("Failed to fetch updates.");
+        }
+        exec(`cd ${projectDir} && git checkout ${branchName} && git pull`, (checkoutError) => {
+            if (checkoutError) {
+                return res.status(500).send(`Failed to checkout ${branchName} branch.`);
+            }
+            exec(`cd ${projectDir} && npm install`, (installError) => {
+                if (installError) {
+                    return res.status(500).send("Failed to install dependencies.");
+                }
+
+                exec(`cd ${projectDir} && pm2 restart ${pm2Name}`, (pm2Error) => {
+                    if (pm2Error) {
+                        return res.status(500).send("Failed to restart app with PM2.");
+                    }
+                    res.send("Application updated, dependencies installed, and restarted.");
+                });
+            });
+        });
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
