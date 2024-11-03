443c578cfaa992c1b774256bf74a3011ed29059c
arya <javdani.arya@gmail.com>
2024-11-03 16:41:16 +0330

    multi and zar added

--- deployer.js
+++ deployer.js
@@ -33,6 +33,62 @@ app.get("/deploy/shop", (req, res) => {
     });
 });
 
+app.get("/deploy/zar", (req, res) => {
+    const pm2Name = 'zar'
+    const branchName = 'back-test';
+    const projectDir = "/home/net/zar";
+    exec(`cd ${projectDir} && git fetch`, (fetchError) => {
+        if (fetchError) {
+            return res.status(500).send("Failed to fetch updates.");
+        }
+        exec(`cd ${projectDir} && git checkout ${branchName} && git pull`, (checkoutError) => {
+            if (checkoutError) {
+                return res.status(500).send(`Failed to checkout ${branchName} branch.`);
+            }
+            exec(`cd ${projectDir} && npm install --force`, (installError) => {
+                if (installError) {
+                    return res.status(500).send("Failed to install dependencies.");
+                }
+
+                exec(`cd ${projectDir} && pm2 restart ${pm2Name}`, (pm2Error) => {
+                    if (pm2Error) {
+                        return res.status(500).send("Failed to restart app with PM2.");
+                    }
+                    res.send("Application updated, dependencies installed, and restarted.");
+                });
+            });
+        });
+    });
+});
+
+app.get("/deploy/multi", (req, res) => {
+    const pm2Name = 'multi'
+    const branchName = 'back-test';
+    const projectDir = "/home/net/multi";
+    exec(`cd ${projectDir} && git fetch`, (fetchError) => {
+        if (fetchError) {
+            return res.status(500).send("Failed to fetch updates.");
+        }
+        exec(`cd ${projectDir} && git checkout ${branchName} && git pull`, (checkoutError) => {
+            if (checkoutError) {
+                return res.status(500).send(`Failed to checkout ${branchName} branch.`);
+            }
+            exec(`cd ${projectDir} && npm install --force`, (installError) => {
+                if (installError) {
+                    return res.status(500).send("Failed to install dependencies.");
+                }
+
+                exec(`cd ${projectDir} && pm2 restart ${pm2Name}`, (pm2Error) => {
+                    if (pm2Error) {
+                        return res.status(500).send("Failed to restart app with PM2.");
+                    }
+                    res.send("Application updated, dependencies installed, and restarted.");
+                });
+            });
+        });
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
