a416ebb9b736575214f28bc6cfca4c9a5f7ffed3
arya <javdani.arya@gmail.com>
2024-11-12 13:39:31 +0330

    pars

--- deployer.js
+++ deployer.js
@@ -62,7 +62,7 @@ app.get("/deploy/zar", (req, res) => {
 });
 
 app.get("/deploy/zar-front", (req, res) => {
-    const pm2Name = 'zar-front  '
+    const pm2Name = 'zar-front'
     const branchName = 'panel-test';
     const projectDir = "/home/front/zaradmin";
     exec(`cd ${projectDir} && git fetch`, (fetchError) => {
@@ -93,6 +93,34 @@ app.get("/deploy/multi", (req, res) => {
     const pm2Name = 'multi'
     const branchName = 'back-test';
     const projectDir = "/home/net/multi";
+    exec(`cd ${projectDir} && git fetch`, (fetchError) => {
+        if (fetchError) {
+            return res.status(500).send("Failed to fetch updates.");
+        }
+        exec(`cd ${projectDir} && git checkout ${branchName} && git pull`, (checkoutError) => {
+            if (checkoutError) {
+                return res.status(500).send(`Failed to checkout ${branchName} branch.`);
+            }
+            exec(`cd ${projectDir} && npm install --force`, (installError) => {
+                if (installError) {
+                    return res.status(500).send("Failed to install dependencies.");
+                }
+
+                exec(`cd ${projectDir} && pm2 restart ${pm2Name}`, (pm2Error) => {
+                    if (pm2Error) {
+                        return res.status(500).send("Failed to restart app with PM2.");
+                    }
+                    res.send("Application updated, dependencies installed, and restarted.");
+                });
+            });
+        });
+    });
+});
+
+app.get("/deploy/pars", (req, res) => {
+    const pm2Name = 'pars-test'
+    const branchName = 'back-test';
+    const projectDir = "/home/net/pars";
     exec(`cd ${projectDir} && git fetch`, (fetchError) => {
         if (fetchError) {
             return res.status(500).send("Failed to fetch updates.");
